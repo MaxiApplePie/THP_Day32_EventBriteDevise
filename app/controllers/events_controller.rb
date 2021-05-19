4331a881b17f88a7a938447a359c1a9afd8fea85
Laurent Mollard-Chaumette <lmc@outlook.fr>
2021-05-19 17:02:18 +0200

    Remise à niveau des fonctionnalités manquantes de mardi

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,39 @@
 class EventsController < ApplicationController
   
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
+    @start_date = @event.start_date.to_date.strftime('%d %m %Y')
+    # @end_date = @event.end_date.to_date.strftime('%d %m %Y')
   end
-  
+
   def new
+  end
+
+  def create
+    @start_date = DateTime.strptime(params[:start_date], "%Y-%m-%d") 
+    @event = Event.create(
+      title: params[:title],
+      description: params[:description],
+      start_date: @start_date,
+      duration: params[:duration],
+      location: params[:location],
+      price: params[:price].to_i,
+      organiser: current_user
+    )
+
+    if @event.save
+      redirect_to event_path(@event.id), alert: "Evènement créé avec succès"
+    else
+      p @event.errors.messages
+      flash.now[:alert] = "Problème lors de la création de l'évènement"
+      render :new
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'events#index'
+  devise_for :users
   get 'static_pages/index'
   get 'static_pages/secret'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
+  resources :users, only: [:show]
 end
